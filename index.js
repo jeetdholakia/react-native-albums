49a7316dab63f2253575b8ec5d48b72fef059c4e
Jeet Dholakia <onemediasoft@gmail.com>
2018-10-26 12:47:27 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+//Import a library to create a component
+
+//Create a component
+
+//Render the component
